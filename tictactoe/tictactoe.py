c436a31942e88eb5a79030f28352cd249748b76b
vts <saschavv@gmail.com>
2023-06-15 23:18:46 +0200

    Cleanup routines.
    Reuse min_value and max_value in the minmax routine.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -54,24 +54,14 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    if isinstance(action, list):
-        raise Exception("Invalid type")
-    if len(action) != 2:
-        raise Exception("Action should be a coordinate")
     i = action[0]
     j = action[1]
-    if i < 0 or i > 2:
-        raise Exception("Row value out of bounds")
-    if j < 0 or j > 2:
-        raise Exception("Column value out of bounds")
 
     if board[i][j] != EMPTY:
         raise Exception("Invalid action, postion already taken")
 
     local_board = copy.deepcopy(board)
-
     current_player = player(board)
-
     local_board[i][j] = current_player
 
     return local_board
@@ -81,20 +71,20 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    for p in [X, O]:
-        # row winner
-        for i in range(0, 3):
-            if board[i][0] == p and board[i][1] == p and board[i][2] == p:
-                return p
-        # column winner
-        for j in range(0, 3):
-            if board[0][j] == p and board[1][j] == p and board[2][j] == p:
-                return p
-        # orthogonal
-        if board[0][0] == p and board[1][1] == p and board[2][2] == p:
-            return p
-        if board[0][2] == p and board[1][1] == p and board[2][0] == p:
-            return p
+    
+    # row winner
+    for i in range(0, 3):
+        if board[i][1] == board[i][0] and board[i][2] == board[i][0]:
+            return board[i][0]
+    # column winner
+    for j in range(0, 3):
+        if board[1][j] == board[0][j] and board[2][j] == board[0][j]:
+            return board[0][j]
+    # orthogonal
+    if board[1][1] == board[0][0] and board[2][2] == board[0][0]:
+        return board[0][0]
+    if board[1][1] == board[0][2] and board[2][0] == board[0][2]:
+        return board[0][2]
     return None
 
 
@@ -126,20 +116,28 @@ def utility(board):
 
 def max_value(board):
     if terminal(board):
-        return utility(board)
+        return utility(board), None
     v = -math.inf
+    a = None
     for action in actions(board):
-        v = max([v, min_value(result(board, action))])
-    return v
+        local_v = min_value(result(board, action))[0]
+        if local_v > v:
+            v = local_v
+            a = action
+    return v,a
 
 
 def min_value(board):
     if terminal(board):
-        return utility(board)
+        return utility(board), None
     v = math.inf
+    a = None
     for action in actions(board):
-        v = min([v, max_value(result(board, action))])
-    return v
+        local_v = max_value(result(board, action))[0]
+        if local_v < v:
+            v = local_v
+            a = action
+    return v,a
 
 
 def minimax(board):
@@ -148,25 +146,9 @@ def minimax(board):
     """
     if terminal(board):
         return None
-
-    optimal = None
+    
     if player(board) == X:
-        v = -math.inf
-        
-        for action in actions(board):
-            result_board = result(board, action)
-            value = min_value(result_board)
-            if value > v:
-                v = value
-                optimal = action
+        return max_value(board)[1]
     else:
-        v = math.inf
+        return min_value(board)[1]
         
-        for action in actions(board):
-            result_board = result(board, action)
-            value = max_value(result_board)
-            if value < v:
-                v = value
-                optimal = action
-
-    return optimal
